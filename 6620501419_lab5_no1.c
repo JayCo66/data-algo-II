cf093b3da51cac784dc6087bf9569a323daedbaa
JayCo66 <boomtnv.dev@gmail.com>
2025-12-25 00:14:58 +0700

    update

--- 6620501419_lab5_no1.c
+++ 6620501419_lab5_no1.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 
-int X[] = {5, 10, 20, 30, 30};
-int n = sizeof(X) / sizeof(X[0]);
+int main(){
+    int X[] = {5,10,20,30,30};
+    int n = sizeof(X)/sizeof(X[0]);
+    int cost = 0;
 
-int optimalMerge(int X[], int n) {
-    int totalCost = 0;
+    while(n>1){
+        int mergecost = X[0] + X[1];
+        cost += mergecost;
 
-    while (n > 1) {
-        int first = X[0];
-        int second = X[1];
-        int newSize = first + second;
+        for(int i=2; i<n; i++){
+            X[i-2] = X[i];
+        }
+        n -= 2;
 
-        totalCost += newSize;
-        
-        int inserted = 0;
-        int r = 2;
-        
-        for (int i = 0; i < n - 1; i++) {
-            if (!inserted && (r == n || newSize < X[r])) {
-                X[i] = newSize;
-                inserted = 1;
-            } else {
-                X[i] = X[r];
-                r++;
+        int pos = n;
+        for(int i=0; i<n; i++){
+            if (mergecost <= X[i]){
+                pos = i;
+                break;
             }
         }
-        n--;
+
+        for(int i = n; i > pos; i--){
+            X[i] = X[i-1];
+        }
+        X[pos] = mergecost;
+        n++;
     }
 
-    return totalCost;
-}
-
-int main() {
-    printf("Total Cost = %d", optimalMerge(X, n));
-    return 0;
+    printf("total cost = %d", cost);
 }
